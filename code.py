2b3f2af1b84132a57c790943c31c6e249c3663a6
temtemtemidoit <131671020+temtemtemidoit@users.noreply.github.com>
2023-05-29 20:37:35 +0800

    Update code.py
    updated

--- code.py
+++ code.py
@@ -1,3 +1,29 @@
-import os
-while True:
-    os.system("start")
+import pygame
+
+# Define window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Initialize Pygame
+pygame.init()
+
+# Create windows until the user closes them
+running = True
+while running:
+    # Create a new Pygame window
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # If the user closes the window, stop the program
+            running = False
+    
+    # Fill the window with a color
+    window.fill((255, 255, 255))
+    
+    # Update the contents of the window
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
